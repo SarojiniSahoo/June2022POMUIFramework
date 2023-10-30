7f4e52847840ef619f61528ceadbd6bf69a97ff7
Nitya <nitya@gmail.com>
2023-10-30 17:03:53 +0530

    test: small change

--- src/main/java/com/qa/opencart/constants/AppConstants.java
+++ src/main/java/com/qa/opencart/constants/AppConstants.java
@@ -1,7 +1,7 @@
 package com.qa.opencart.constants;
 
 public class AppConstants {
-	public static final int DEFAULT_TIME_OUT=7;
+	public static final int DEFAULT_TIME_OUT=5;
 	public static final int DEFAULT_LARGE_TIME_OUT=10;
 	public static final String LOGIN_PAGE_TITLE="Account Login";
 	public static final String ACC_PAGE_TITLE="My Account11";
